c17cad987d3403f6bed6541caf9f83db0084e6e3
Hanna <79318688+Honey-lee429@users.noreply.github.com>
2024-03-05 21:31:35 -0300

    Create EvenOdd.java

---
+++ EvenOdd.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+public class EvenOdd {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        var s = Integer.parseInt(bufferedReader.readLine());
+        boolean even = s % 2 == 0;
+        var odd = s % 2 != 0;
+        if (odd) {
+            System.out.println("Weird");
+        } else if (even && s >= 2 && s <= 5) {
+            System.out.println("Not Weird");
+        } else if (even && s >= 6 && s <= 20) {
+            System.out.println("Weird");
+        } else {
+            System.out.println("Not Weird");
+        }
+
+
+    }
+}
