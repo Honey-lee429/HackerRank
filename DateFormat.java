90586a6109f3d445b4c84587321cda843aea8b9c
Hanna <79318688+Honey-lee429@users.noreply.github.com>
2024-03-13 07:08:41 -0300

    Create DateFormat.java

---
+++ DateFormat.java
@@ -0,0 +1,12 @@
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class DateFormat {
+
+    public static void main(String[] args) {
+        String pattern = "E, dd MMM yyyy HH:mm:ss z";
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat(pattern);
+        String date = simpleDateFormat.format(new Date());
+        System.out.println(date);
+    }
+}
